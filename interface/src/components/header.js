dac68b5cd6f74c391350acc5a5441fb64cfea21c
climax-solution <blocklimax@gmail.com>
2022-04-07 20:53:42 +0300

    fix Link

--- interface/src/components/header.js
+++ interface/src/components/header.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { a } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Breakpoint, { BreakpointProvider, setDefaultBreakpoints } from "react-socks";
 
 setDefaultBreakpoints([
